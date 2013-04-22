54ce8c6c28d807a855a051fec0dfebcd8f324c3c
Mat Schaffer <mat@schaffer.me>
2013-04-22 16:50:41 -0700

    A version switcher

---
+++ versions.zsh
@@ -0,0 +1,6 @@
+switch_grails() {
+  GRAILS_VERSION="${1:-2.2.1}"
+  export GRAILS_HOME="/usr/local/Cellar/grails/${GRAILS_VERSION}/libexec"
+  [[ -d "$GRAILS_HOME" ]] || echo "WARNING: Requested version of grails was not found"
+  alias grails="${GRAILS_HOME}/bin/grails"
+}
