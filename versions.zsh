251147ff4427126a1a451a5f61bead0ed945f9cc
Mat Schaffer <matschaffer@netflix.com>
2013-05-01 16:05:05 -0700

    Really switch to 2.2.2

--- versions.zsh
+++ versions.zsh
@@ -1,5 +1,5 @@
 switch_grails() {
-  GRAILS_VERSION="${1:-2.2.1}"
+  GRAILS_VERSION="${1:-2.2.2}"
   export GRAILS_HOME="/usr/local/Cellar/grails/${GRAILS_VERSION}/libexec"
   [[ -d "$GRAILS_HOME" ]] || echo "WARNING: Requested version of grails was not found"
   alias grails="${GRAILS_HOME}/bin/grails"
